a21d1d325a7dba815be22e0f85ed542802d4425c
Harish18200 <harishkutty300@gmail.com>
2025-07-28 19:52:04 +0530

    FizrApp setup

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize('fizrapp', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+  logging: false,
+});
+
+module.exports = sequelize;
+
+sequelize.authenticate()
+    .then(() => {
+        console.log("Connected to MySQL Database");
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+module.exports = sequelize;
+
+
